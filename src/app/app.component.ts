614b09eeb80e271e0a732b845051c2595d44d685
alexey-kozlenkov <nidecker95@gmail.com>
2018-07-05 20:15:54 +0300

    (demo #2): - Demonstrate interpolation - Demonstate ngFor - Demonstrate ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  titles = Array(10).fill('Photo Feed #nitro #3000');
 }
