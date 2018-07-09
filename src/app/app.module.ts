4189df407406b1ef4f744ea8e3ba7e5f78f09178
alexey-kozlenkov <nidecker95@gmail.com>
2018-07-09 14:56:09 +0300

    Add directive example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { PhotoPostComponent } from './photo-post/photo-post.component';
 import { PhotoPostFeedComponent } from './photo-post-feed/photo-post-feed.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ZoomImgDirective } from './photo-post/img-zoom.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     PhotoPostComponent,
-    PhotoPostFeedComponent
+    PhotoPostFeedComponent,
+    ZoomImgDirective
   ],
   imports: [
     BrowserModule,
